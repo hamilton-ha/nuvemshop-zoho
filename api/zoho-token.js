84b09c64d3609465d0ed090a22b28238e5589bde
hamilton-ha <soaresjrhamilton@gmail.com>
2026-04-23 23:12:44 -0300

    Update zoho-token.js

--- api/zoho-token.js
+++ api/zoho-token.js
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
       client_id: "1000.GIMYXWBM45YXE5EI4Q6JILEK5CH86R",
       client_secret: "f28096d07558b6c277a0d5551260c1be5e8f006a5c",
       redirect_uri: "https://www.zoho.com",
-      code: "1000.102e99329342b5fdf493333a7222ef13.058cb396b89b6c48cc01c47615924386"
+      code: "1000.ad3b3bf5b0b9a141e2cdddd9b9f04ea9.07c8ff3f3c967a56392b5a669a679721"
     })
   });
 
