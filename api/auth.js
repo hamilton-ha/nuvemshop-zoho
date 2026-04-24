08f184b94426d5630cc124ce50775561d209e580
hamilton-ha <soaresjrhamilton@gmail.com>
2026-04-23 21:39:20 -0300

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -1,15 +1,29 @@
-export default function handler(req, res) {
-  const { code, store_id } = req.query;
-
-  console.log('Query recebida:', req.query);
+export default async function handler(req, res) {
+  const { code } = req.query;
 
   if (!code) {
-    return res.status(200).send('Instalação iniciada, aguardando código...');
+    return res.status(200).send('Instalação iniciada...');
   }
 
+  const response = await fetch('https://www.nuvemshop.com.br/apps/authorize/token', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      client_id: '30397',
+      client_secret: '4532c203f6a247c5b8270730ec781cc5e219b66b0d1969d1',
+      grant_type: 'authorization_code',
+      code: code
+    })
+  });
+
+  const data = await response.json();
+
+  console.log('TOKEN RECEBIDO:', data);
+
   return res.status(200).send(`
-    <h1>Integração funcionando 🚀</h1>
-    <p>Code: ${code}</p>
-    <p>Store ID: ${store_id}</p>
+    <h1>Token gerado 🚀</h1>
+    <pre>${JSON.stringify(data, null, 2)}</pre>
   `);
 }
