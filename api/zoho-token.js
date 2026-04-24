1101b1b3b86db99679ad3a598869a0a8a6bbf9af
hamilton-ha <soaresjrhamilton@gmail.com>
2026-04-23 23:25:07 -0300

    Update zoho-token.js

--- api/zoho-token.js
+++ api/zoho-token.js
@@ -1,4 +1,10 @@
 export default async function handler(req, res) {
+  const { code } = req.query;
+
+  if (!code) {
+    return res.status(200).send("Aguardando code do Zoho...");
+  }
+
   const response = await fetch("https://accounts.zoho.com/oauth/v2/token", {
     method: "POST",
     headers: {
@@ -8,8 +14,8 @@ export default async function handler(req, res) {
       grant_type: "authorization_code",
       client_id: "1000.GIMYXWBM45YXE5EI4Q6JILEK5CH86R",
       client_secret: "f28096d07558b6c277a0d5551260c1be5e8f006a5c",
-      redirect_uri: "https://www.zoho.com",
-      code: "1000.61b01a2ad355f95a4f5d5f0c175c0d84.16d7a1f62f63719b658a7b41cbc9f4b0"
+      redirect_uri: "https://project-2jpn7.vercel.app/api/zoho-token",
+      code: code
     })
   });
 
