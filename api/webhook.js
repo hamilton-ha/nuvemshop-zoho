30b3924812de425f6bb652372828bb038f6eaed3
hamilton-ha <soaresjrhamilton@gmail.com>
2026-04-23 22:12:22 -0300

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   const token = "a687e51c0c454e0f89fe239db9c808d31d2bf15a";
 
-  const response = await fetch("https://api.nuvemshop.com.br/v1/4882514/customers", {
+  const response = await fetch("https://api.nuvemshop.com.br/v1/4882514/customers?page=1&per_page=200", {
     headers: {
       "Authentication": `bearer ${token}`,
       "User-Agent": "Elo Forte App (contato@elofortedigital.com.br)",
